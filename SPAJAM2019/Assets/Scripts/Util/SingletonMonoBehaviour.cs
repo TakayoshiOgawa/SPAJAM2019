d1b7f3c0722f5eec2513ef2de6ada3f8d1c2c732
ogawa.t <ogawa4283gamecreator@gmail.com>
2019-06-13 21:52:17 +0900

    SingletonMonoBehaviourを追加

---
+++ SPAJAM2019/Assets/Scripts/Util/SingletonMonoBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    [Header("Singleton"), SerializeField]
+    private bool dontDestoryOnLoad = false;
+
+    /// <summary>
+    /// 静的なインスタンス
+    /// </summary>
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (!HasInstance())
+            {
+                FindInstance();
+            }
+            return instance;
+        }
+    }
+
+    /// <summary>
+    /// インスタンスが存在するか
+    /// </summary>
+    public static bool HasInstance()
+    {
+        return instance != null;
+    }
+
+    /// <summary>
+    /// シーン内のインスタンスを検索
+    /// </summary>
+    static protected void FindInstance()
+    {
+        instance = FindObjectOfType<T>();
+        if (instance == null)
+        {
+            Debug.LogError("Not Find a " + typeof(T) + " instance");
+        }
+    }
+
+    /// <summary>
+    /// 生成時に実行
+    /// </summary>
+    protected virtual void Awake()
+    {
+        // 他のゲームオブジェクトにアタッチされているか調べる
+        // アタッチされている場合は破棄する。
+        if (Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        // シングルトンとして破棄されないオブジェクトに登録
+        if (dontDestoryOnLoad)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
