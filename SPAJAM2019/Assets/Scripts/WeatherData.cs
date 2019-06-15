e147e9c37b4b063ac562e68bda527e06b1dd4a23
ogawa.t <ogawa4283gamecreator@gmail.com>
2019-06-16 01:16:59 +0900

    天気の名称に日本語を返す

--- SPAJAM2019/Assets/Scripts/WeatherData.cs
+++ SPAJAM2019/Assets/Scripts/WeatherData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Weather
 {
-	public struct WeatherData
+    public struct WeatherData
 	{
 		public double temp;
 		public double tempMin;
@@ -12,5 +12,31 @@ namespace Weather
 		public double humidity;
 		public double windSpeed;
 		public string icon;
+
+        public string WeatherJP()
+        {
+            // 日本語名（全てではない
+            var jp_dic = new Dictionary<string, string>()
+            {
+                { "thunderstorm ", "雷" },
+                { "drizzle", "雨" },
+                { "rain", "雨" },
+                { "snow", "雪" },
+                { "clear", "晴れ" },
+                { "clouds", "雲" },
+            };
+
+            // 天気の名称に含まれるものがあれば日本語で返す
+            foreach (var jp in jp_dic)
+            {
+                if(weather.Contains(jp.Key))
+                {
+                    return jp.Value;
+                }
+            }
+
+            // 見つからない場合は英語のまま返す
+            return weather;
+        }
 	}
 }
