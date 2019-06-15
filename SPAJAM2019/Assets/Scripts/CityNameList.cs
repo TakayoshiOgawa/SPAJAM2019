a38274e8bc32cecae9897a82cefc120067d620c9
KazunariYamada <k1z0n1r7@yahoo.co.jp>
2019-06-16 01:00:44 +0900

    コメントしてあったのを外しました

--- SPAJAM2019/Assets/Scripts/CityNameList.cs
+++ SPAJAM2019/Assets/Scripts/CityNameList.cs
@@ -16,15 +16,15 @@ public enum City
 
 public class CityNameList
 {
-	//public static Dictionary<City, string> cityList = new Dictionary<City, string>()
-	//{
-	//	{ City.Gifu, "Ogaki" },
-	//	{ City.Nagoya, "Nagoya" },
-	//	{ City.Mie, "Yokkaichi" },
-	//	{ City.Sapporo, "Sapporo" },
-	//	{ City.Tokyo, "Tokyo" },
-	//	{ City.Osaka, "Osaka" },
-	//	{ City.Okinawa, "Naha" },
-	//};
+	public static Dictionary<City, string> cityList = new Dictionary<City, string>()
+	{
+		{ City.Gifu, "Ogaki" },
+		{ City.Nagoya, "Nagoya" },
+		{ City.Mie, "Yokkaichi" },
+		{ City.Sapporo, "Sapporo" },
+		{ City.Tokyo, "Tokyo" },
+		{ City.Osaka, "Osaka" },
+		{ City.Okinawa, "Naha" },
+	};
 }
 
